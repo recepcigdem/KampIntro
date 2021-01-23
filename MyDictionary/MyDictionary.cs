46557bb3a4c5f96b45b756d56291782646d5867c
recepcigdem <recepcigdem24@gmail.com>
2021-01-23 17:52:43 +0300

    Dictionary Added!

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    public class MyDictionary<K,V>
+    {
+        public K[] keys;
+        public V[] values;
+
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K key, V value)
+        {
+            K[] tempKeys = keys;
+            V[] tempValues = values;
+
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDict = new MyDictionary<int, string>();
+
+            myDict.Add(34, "İstanbul");
+            myDict.Add(6, "Ankara");
+            myDict.Add(35, "İzmir");
+            myDict.Add(54, "Sakarya");
+            myDict.Add(16, "Bursa");
+
+            for (int i = 0; i < myDict.keys.Length; i++)
+            {
+                Console.WriteLine(myDict.keys[i] + " -> " + myDict.values[i]);
+            }
+
+        }
+    }
+}
