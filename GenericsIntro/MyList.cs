e2215dfadb6cb14dc165ebce3b277fce9ac5c611
recepcigdem <recepcigdem24@gmail.com>
2021-01-23 17:32:22 +0300

    deneme

--- GenericsIntro/MyList.cs
+++ GenericsIntro/MyList.cs
@@ -7,14 +7,14 @@ namespace GenericsIntro
     class MyList<T>
     {
         T[] items;
-
+        T[] tempArray;
         public MyList()
         {
             items = new T[0];
         }
         public void Add(T item)
-        {
-            T[] tempArray = items;
+        { 
+            tempArray = items;
             items = new T[items.Length + 1];
 
             for (int i = 0; i < tempArray.Length; i++)
